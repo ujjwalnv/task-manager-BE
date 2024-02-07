f14dd65b9c78c2689ac4f608d1d993926e681e80
Ujjwal Verma <vujjwal1n@gmail.com>
2024-02-07 18:42:40 +0530

    Wrote unit tests for Get Filter Tasks API

--- test/tasks.test.ts
+++ test/tasks.test.ts
@@ -2,23 +2,23 @@ import request from 'supertest'
 import {taskManagerApp} from '../src/index'; // Assuming your Express app is exported from app.ts
 
 describe('Create Task API', () => {
-//   it('should create a new task', async () => {
-//     const taskData = {
-//       title: 'Sample Task 1234',
-//       description: 'This is a sample task',
-//       dueDate: '2024-02-07T12:00:00Z',
-//       assignees: [1, 2],
-//     };
+  it('should create a new task', async () => {
+    const taskData = {
+      title: 'Sample Task 1234',
+      description: 'This is a sample task',
+      dueDate: '2024-02-07T12:00:00Z',
+      assignees: [1, 2],
+    };
 
-//     const response = await request(taskManagerApp)
-//       .post('/tasks')
-//       .set('creator_id', '3')
-//       .send(taskData)
-//       .expect(200);
+    const response = await request(taskManagerApp)
+      .post('/tasks')
+      .set('creator_id', '3')
+      .send(taskData)
+      .expect(200);
 
-//     // Verifying if provided title is same as the response title
-//     expect(response.body.title).toBe(taskData.title);
-//   });
+    // Verifying if provided title is same as the response title
+    expect(response.body.title).toBe(taskData.title);
+  });
 
   it('should return 400 if required fields are missing', async () => {
     const invalidTaskData = {
@@ -34,14 +34,14 @@ describe('Create Task API', () => {
 
 describe('Get Assigned Tasks API', () => {
     it('should give assigned tasks for the user_id',async () => {
-        const response = await request(taskManagerApp)
+        await request(taskManagerApp)
           .get('/tasks/assigned')
           .set('user_id', '2')
           .expect(200)
     })
 
     it('should return 400 if required fields are missing',async () => {
-        const response = await request(taskManagerApp)
+        await request(taskManagerApp)
           .get('/tasks/assigned') // Removed user_id from header
           .expect(400)
     })
@@ -49,15 +49,59 @@ describe('Get Assigned Tasks API', () => {
 
 describe('Get Created Tasks API', () => {
     it('should give created tasks for the user_id',async () => {
-        const response = await request(taskManagerApp)
+        await request(taskManagerApp)
           .get('/tasks/created')
           .set('user_id', '1')
           .expect(200)
     })
 
     it('should return 400 if required fields are missing',async () => {
-        const response = await request(taskManagerApp)
+        await request(taskManagerApp)
           .get('/tasks/created') // Removed user_id from header
           .expect(400)
+    })
+})
+
+describe('Get Filter Tasks API', () => {
+    it('should give filtered tasks for the query params provided each query params',async () => {
+        await request(taskManagerApp)
+          .get('/tasks/filter')
+          .query({
+            dueDate: '2024-02-07T12:00:00.000Z',
+            creatorId: '3', 
+            assigneeId: '2'
+          })
+          .expect(200)
+    })
+
+    it('should give filtered tasks for the query params provided partial query params',async () => {
+        await request(taskManagerApp)
+          .get('/tasks/filter')
+          .query({
+            creatorId: '3', 
+            assigneeId: '2'
+          })
+          .expect(200)
+
+          await request(taskManagerApp)
+          .get('/tasks/filter')
+          .query({
+            creatorId: '3', 
+            assigneeId: '2'
+          })
+          .expect(200)
+
+          await request(taskManagerApp)
+          .get('/tasks/filter')
+          .query({
+            assigneeId: '2'
+          })
+          .expect(200)
+    })
+
+    it('should give filtered tasks for the query params provided no query params',async () => {
+        await request(taskManagerApp)
+          .get('/tasks/filter')
+          .expect(200)
     })
 })
